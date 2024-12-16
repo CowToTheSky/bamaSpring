93825c37d3c1f72baf4d735a8185c2e0aa47008f
junbaba <junbaba@qq.com>
2024-12-16 23:12:37 +0800

    实际上只要set满足驼峰就能触发自动装配

--- src/main/java/org/example/bean/Student.java
+++ src/main/java/org/example/bean/Student.java
@@ -2,6 +2,7 @@ package org.example.bean;
 
 public class Student {
     private Teacher teacher;
+    //实际上只要set后字母大写，就可以实现自动装配
     public void setTeacher(Teacher teacher) {
         this.teacher = teacher;
     }
